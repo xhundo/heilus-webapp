bed56c9f4911b73d8c96b79b2c78481f5d786d37
Kurtney Joseph <josephkurt18@gmail.com>
2022-09-04 18:17:27 -0400

    add popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,18 +1,35 @@
 class Popup {
-  constructor({ popupSelector, name, job, about }) {
-    this.popupSelector = popupSelector;
-    this.name = name;
-    this.job = job;
-    this.about = about;
+  constructor({ popupSelector, button }) {
+    this._popupElement = document.querySelector(`${popupSelector}`);
+    this._buttonElement = document.querySelector(`${button}`);
     this._button = document.querySelector(".popup__button");
+    this.popup = this.popup;
   }
 
-  open() {}
+  open() {
+    this._popupElement.classList.add("popup_open");
+  }
 
-  close() {}
+  close() {
+    this._popupElement.classList.remove("popup_open");
+  }
 
-  setEventListener() {
-    this._button.addEventListener("click", this.close);
+  setEventListeners() {
+    this._button.addEventListener("click", () => {
+      this.close();
+    });
+    this._buttonElement.addEventListener("click", () => {
+      this.open();
+    });
+  }
+
+  fillInfo(name, job, about) {
+    this.name = document.querySelector("#name");
+    this.name.textContent = name;
+    this.job = document.querySelector("#job");
+    this.job.textContent = job;
+    this.about = document.querySelector(".popup__text");
+    this.about.textContent = about;
   }
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,18 @@
 import "../pages/index.css";
-import "../components/Popup.js";
+import Popup from "../components/Popup.js";
+
+const popup = new Popup({ popupSelector: "#popup", button: "#button1" });
+popup.fillInfo(
+  "Ani Petrichor",
+  "Data Analyst",
+  "Ani Petrichor is a Brooklyn-based data analyst. She has a background in operations & logistics in the sustainability food sector, primarily working to bring locally farmed ingredients to urban residents. She believes that big data can be used to effect good in the world."
+);
+
+const popup2 = new Popup({
+  popupSelector: "#popup",
+  button: "#button2",
+});
+popup2.fillInfo("Jo");
+
+popup.setEventListeners();
+popup2.setEventListeners();
